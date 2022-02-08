5c78f1010e14b8c11191bdd62ad4e8dfa81f3908
Ondřej Tůma <ondr@centrum.cz>
2022-02-08 16:41:23 +0100

    handled some ts errors

---
+++ src/components/client/Calendar/Calendar.tsx
@@ -0,0 +1,87 @@
+import { useMemo } from 'react'
+import { calendar_v3 } from 'googleapis'
+import { Calendar as ReactCalendar, dateFnsLocalizer, SlotInfo, Event } from 'react-big-calendar'
+import format from 'date-fns/format'
+import getDay from 'date-fns/getDay'
+import cs from 'date-fns/locale/cs'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import sub from 'date-fns/sub'
+import Schema$Event = calendar_v3.Schema$Event
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales: {
+    cs,
+  },
+})
+
+const messages = {
+  allDay: 'Celý den',
+  previous: '<',
+  next: '>',
+  today: 'Dnes',
+  month: 'Měsíc',
+  week: 'Týden',
+  day: 'Den',
+  agenda: 'Agenda',
+  date: 'Datum',
+  time: 'Čas',
+  event: 'Událost',
+  showMore: (total: number) => `+ Zobrazit další (${total})`,
+}
+
+const parseGoogleCalendarEventsToReactCalendarEvents = (events: Schema$Event[]) => events.map(({
+  description,
+  end,
+  extendedProperties,
+  summary,
+  start,
+}) => ({
+  title: summary,
+  start: start?.date || start?.dateTime,
+  end: end?.date ? sub(new Date(end.date), { days: 1 }) : end?.dateTime,
+  allDay: !!start?.date,
+  resource: {
+    private: extendedProperties?.private,
+    description,
+  },
+}))
+
+export type onSelectEventType = (event: Event) => void
+export type onSelectSlotType = (slotInfo: SlotInfo) => void
+
+type CalendarProps = {
+  events: Schema$Event[],
+  onSelectEvent: onSelectEventType,
+  onSelectSlot: onSelectSlotType,
+}
+
+const Calendar = ({ events, onSelectEvent, onSelectSlot }: CalendarProps) => {
+  const reservations: Event[] = useMemo(() => parseGoogleCalendarEventsToReactCalendarEvents(events), [ events ])
+
+  return (
+    <div>
+      <ReactCalendar
+        culture={'cs'}
+        localizer={localizer}
+        events={reservations}
+        startAccessor="start"
+        endAccessor="end"
+        onSelectEvent={onSelectEvent}
+        views={[ 'month' ]}
+        style={{ height: 700 }}
+        messages={messages}
+        popup
+        // popupOffset={{x: 30, y: 20}}
+        selectable
+        onSelectSlot={onSelectSlot}
+      />
+    </div>
+  )
+}
+
+export default Calendar
