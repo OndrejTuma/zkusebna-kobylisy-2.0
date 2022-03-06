e7e953bd5b21d03aeb4fd16b811faf18fdda2356
Ondřej Tůma <ondr@centrum.cz>
2022-03-06 18:21:13 +0100

    cover case when there are no events

--- src/components/client/Calendar/Calendar.tsx
+++ src/components/client/Calendar/Calendar.tsx
@@ -1,12 +1,12 @@
-import { useMemo } from 'react'
-import { calendar_v3 } from 'googleapis'
-import { Calendar as ReactCalendar, dateFnsLocalizer, SlotInfo, Event } from 'react-big-calendar'
 import format from 'date-fns/format'
 import getDay from 'date-fns/getDay'
 import cs from 'date-fns/locale/cs'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
 import sub from 'date-fns/sub'
+import { calendar_v3 } from 'googleapis'
+import { useMemo } from 'react'
+import { Calendar as ReactCalendar, dateFnsLocalizer, Event, SlotInfo } from 'react-big-calendar'
 import Schema$Event = calendar_v3.Schema$Event
 
 const localizer = dateFnsLocalizer({
@@ -42,8 +42,8 @@ const parseGoogleCalendarEventsToReactCalendarEvents = (events: Schema$Event[]) 
   start,
 }) => ({
   title: summary,
-  start: start?.date || start?.dateTime,
-  end: end?.date ? sub(new Date(end.date), { days: 1 }) : end?.dateTime,
+  start: start?.date ? new Date(start.date) : start?.dateTime ? new Date(start.dateTime) : undefined,
+  end: end?.date ? sub(new Date(end.date), { days: 1 }) : end?.dateTime ? new Date(end.dateTime) : undefined,
   allDay: !!start?.date,
   resource: {
     private: extendedProperties?.private,
