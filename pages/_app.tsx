efcae05570b30207ffa961545c9e4b76a0d1ac03
Ondřej Tůma <ondr@centrum.cz>
2022-06-29 09:29:38 +0200

    chore: update picasso and add ssr styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,12 @@ import 'Styles/react-big-calendar.css'
 
 function ZkusebnaKobylisy({ Component, pageProps }: AppProps) {
   return (
-    <Provider loadFavicon={false} fixViewport={false}>
+    <Provider
+      loadFavicon={false}
+      fixViewport={false}
+      loadFonts={false}
+      disableClassNamePrefix
+    >
       <Component {...pageProps} />
     </Provider>
   )
---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import { Children } from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import { getServersideStylesheets } from '@toptal/picasso-provider'
+
+
+export default class MyDocument extends Document {
+  static getInitialProps = async (
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> => {
+    const sheets = getServersideStylesheets()
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />),
+        enhanceComponent: Component => props =>
+          sheets.collect(<Component {...props} />)
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: [
+        ...Children.toArray(initialProps.styles),
+        sheets.getStyleElement()
+      ]
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
