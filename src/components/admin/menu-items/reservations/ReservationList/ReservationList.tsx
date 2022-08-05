6c1ddd200b6147ef5dc3652b6718894e46835531
Ondřej Tůma <ondr@centrum.cz>
2022-08-05 21:32:52 +0200

    chore: display price in reservations

--- src/components/admin/menu-items/reservations/ReservationList/ReservationList.tsx
+++ src/components/admin/menu-items/reservations/ReservationList/ReservationList.tsx
@@ -1,21 +1,33 @@
 import EmptyList from 'Components/admin/EmptyList'
 import React from 'react'
-import { Datagrid, List, TextField, ReferenceField, ReferenceArrayField, ChipField, Pagination, SingleFieldList } from 'react-admin'
+import {
+  ChipField,
+  Datagrid,
+  DateField,
+  List, NumberField,
+  Pagination,
+  ReferenceArrayField,
+  ReferenceField,
+  SingleFieldList,
+  TextField,
+} from 'react-admin'
 
 const ReservationList = (props: {}) => {
   return (
     <List {...props} empty={<EmptyList/>}>
-      <Datagrid rowClick={'edit'}>
-        <TextField label={'Název'} source={'reservationName'} />
-        <TextField label={'Jméno'} source={'name'} />
-        <TextField label={'E-mail'} source={'email'} />
-        <TextField label={'Telefon'} source={'phone'} />
-        <ReferenceField label={'Typ rezervace'} reference={'reservation-types'} source={'reservationType'}>
-          <TextField source={'title'} />
+      <Datagrid rowClick="edit">
+        <TextField label="Název" source="reservationName"/>
+        <TextField label="Jméno" source="name"/>
+        <TextField label="E-mail" source="email"/>
+        <TextField label="Telefon" source="phone"/>
+        <NumberField label="Cena" source="price"/>
+        <DateField label="Začátek" source="dateStart"/>
+        <ReferenceField label="Typ rezervace" reference="reservation-types" source="reservationType">
+          <TextField source="title"/>
         </ReferenceField>
-        <ReferenceArrayField label={'Položky'} reference={'items'} source={'itemIds'} perPage={5} pagination={<Pagination />}>
+        <ReferenceArrayField label="Položky" reference="items" source="itemIds" perPage={5} pagination={<Pagination/>}>
           <SingleFieldList>
-            <ChipField source="title" />
+            <ChipField source="title"/>
           </SingleFieldList>
         </ReferenceArrayField>
       </Datagrid>
