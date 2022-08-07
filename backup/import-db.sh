386949ab3fea3c504d07d54ca0348fa4b6bd907a
Ondřej Tůma <ondr@centrum.cz>
2022-08-07 16:02:05 +0200

    fix: import script

--- backup/import-db.sh
+++ backup/import-db.sh
@@ -6,15 +6,21 @@ targetContainer="zkusebna-kobylisy-mongo"
 targetDb="zk"
 [ -z "$2" ] && exportDir="db-backup" || exportDir=$2
 
+# copy tarball to container
+docker cp $tarballFileName $targetContainer:.
+
 # unzip tarball
 tar -xf $tarballFileName
+# and in container
+docker exec $targetContainer tar -xf $(basename $tarballFileName)
 
 # import each of the file
 for backupfile in "$exportDir"/*
 do
-  filename="$(basename backupfile)"
+  filename="$(basename $backupfile)"
   docker exec $targetContainer mongoimport --collection="${filename%.*}" --db=$targetDb --file=$backupfile
 done
 
 # remove backups
 rm -rf $exportDir
+docker exec $targetContainer rm -rf $exportDir $(basename $tarballFileName)
