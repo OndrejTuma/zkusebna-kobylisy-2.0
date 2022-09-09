ecffa2db576f0c9af15e3be08cfd4ee413c9bb69
Ondřej Tůma <ondr@centrum.cz>
2022-09-09 14:38:56 +0200

    chore: resize, compress and remove previous image

---
+++ pages/api/upload/[id].ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import authorizeRequest from 'Utils/api/authorizeRequest'
+import { getLocalFilePath } from 'Utils/api/fileUpload'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { id } = req.query
+
+  console.log('UPLOAD IMAGE', req.method)
+
+  try {
+    switch (req.method) {
+      case 'DELETE': {
+        authorizeRequest(req)
+
+        fs.unlinkSync(getLocalFilePath(id as string))
+  
+        res.status(200).end()
+  
+        break
+      }
+      default:
+        res.status(405).json({ error: 'Method not allowed' })
+    }
+  } catch (error) { 
+    res.status(400).json({ error: error.message })
+  }
+}
