00a022244cbe6f8b5b1866dcf1257fdb0c129672
Ondřej Tůma <tu.ondrej@gmail.com>
2023-01-08 22:26:11 +0100

    feat: mail template (#19)

--- src/lib/mailer/sendNewReservationMail.ts
+++ src/lib/mailer/sendNewReservationMail.ts
@@ -1,13 +1,11 @@
-import { Reservation } from 'LocalTypes'
-import { sendMessage } from './mailer'
-import Payment from 'Lib/payment'
-import format from 'date-fns/format'
+import { Reservation, ReservationItem, ReservationType } from 'LocalTypes'
+import { populateEmailTemplate, sendMessage } from './mailer'
 
 const subject = 'Rezervace byla vytvořena'
 
-const sendNewReservationMail = (reservation: Reservation) => {
-  const { dateStart, dateEnd, email, price, reservationName } = reservation
-  
+const sendNewReservationMail = async (reservation: Reservation, items: ReservationItem[], reservationTypes: ReservationType[]) => {
+  const { dateStart, dateEnd, email, reservationName } = reservation
+
   if (!email) {
     throw new Error('New reservation error: No email provided')
   }
@@ -18,18 +16,13 @@ const sendNewReservationMail = (reservation: Reservation) => {
     throw new Error('New reservation error: No reservation date provided')
   }
 
-  const message = [`<p>Rezervace <strong>${reservationName}</strong> byla úspěšně vytvořena.</p>`]
+  const html = populateEmailTemplate(['Potvrzení', 'Rezervace'], reservation, items, reservationTypes)
 
-  if (price && price > 0) {
-    message.push(`<p>Cena za rezervaci je <strong>${reservation.price} Kč</strong>.</p>`)
-    message.push(`<p>Platbu proveďte na účet <strong>${Payment.getBankAccount()}</strong></p>`)
-    message.push(`<p><img width="150" alt="${Payment.getPaymentMessage(reservationName)}" src="${Payment.getQRPayment(price, reservationName)}" /></p>`)
-    message.push('<p>Nebo hotově správci zkušebny.</p>')
-  }
-
-  message.push(`<p>Termín rezervace: <strong>${format(new Date(dateStart), 'dd.MM. H:mm')} - ${format(new Date(dateEnd), 'dd.MM. H:mm')}</strong></p>`)
-
-  return sendMessage(email, subject, message.join(''))
+  return sendMessage(
+    email,
+    subject,
+    html,
+  )
 }
 
 export default sendNewReservationMail
