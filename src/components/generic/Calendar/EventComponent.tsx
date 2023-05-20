226906cf9cc437c66cbe1a99f47a452d25d3b060
Ondřej Tůma <ondrej.tuma@gmail.com>
2023-05-21 00:24:37 +0200

    feat: add title with date time to calendar event (#52)

--- src/components/generic/Calendar/EventComponent.tsx
+++ src/components/generic/Calendar/EventComponent.tsx
@@ -1,21 +1,32 @@
 import Box from '@mui/material/Box'
 import { EventProps, Event } from 'react-big-calendar'
 
+import formatDateRange from 'Utils/formatDateRange'
+
 const borderRadius = 8
 
-const EventComponent = ({ event, continuesPrior, continuesAfter }: EventProps<Event>) => {
-  const { title } = event
+const EventComponent = ({
+  event,
+  continuesPrior,
+  continuesAfter,
+}: EventProps<Event>) => {
+  const { title, start, end } = event
+
+  const dateRange = start && end ? formatDateRange(start, end) : ''
 
   return (
-    <Box sx={{
-      backgroundColor: 'primary.main',
-      py: 0.5,
-      px: 1,
-      borderTopRightRadius: continuesAfter ? 0 : borderRadius,
-      borderBottomRightRadius: continuesAfter ? 0 : borderRadius,
-      borderTopLeftRadius: continuesPrior ? 0 : borderRadius,
-      borderBottomLeftRadius: continuesPrior ? 0 : borderRadius,
-    }}>
+    <Box
+      title={dateRange}
+      sx={{
+        backgroundColor: 'primary.main',
+        py: 0.5,
+        px: 1,
+        borderTopRightRadius: continuesAfter ? 0 : borderRadius,
+        borderBottomRightRadius: continuesAfter ? 0 : borderRadius,
+        borderTopLeftRadius: continuesPrior ? 0 : borderRadius,
+        borderBottomLeftRadius: continuesPrior ? 0 : borderRadius,
+      }}
+    >
       {title}
     </Box>
   )
