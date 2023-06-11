3fbc7d1099fa7767f74148fb919e196cdb5bcf1d
Ondřej Tůma <ondrej.tuma@gmail.com>
2023-06-11 15:03:25 +0200

    feat(HowToModal): added stepper with gifs (#72)

--- src/components/client/HowToModal/HowToModal.tsx
+++ src/components/client/HowToModal/HowToModal.tsx
@@ -1,37 +1,124 @@
+import Image from 'next/image'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
 import Modal from 'Components/generic/Modal'
+import Stepper, { useStepper } from 'Components/generic/Stepper'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
 
 interface HowToModalProps {
   isOpen: boolean
   onClose: () => void
 }
 
+const steps = ['', '', '', '', '']
+
 const HowToModal = ({ isOpen, onClose }: HowToModalProps) => {
+  const { activeStep, handleNext, handleBack } = useStepper()
+
   return (
     <Modal open={isOpen} onClose={onClose}>
       <Modal.Title>Jak vytvořit novou rezervaci?</Modal.Title>
       <Modal.Content>
-        <ol>
-          <li>
-            V kalendáři zjistěte, zda váš termín nekoliduje s jinou rezervací.
-          </li>
-          <li>Zvolte si datum a čas rezervace. Více dnů můžete označit tažením myši.</li>
-          <li>
-            Vyplňte všechny požadované údaje jako název akce (např. svatba
-            Josefky Novákové), jméno a kontakt toho, kdo si rezervuje a účel
-            rezervace.
-          </li>
-          <li>
-            Vyberte si položky (u položek uvidíte, zda je možné je rezervovat).
-          </li>
-          <li>Rezervaci odešlete a vyčkejte na její schválení.</li>
-          <li>
-            Případnou platbu poukazujte na účet číslo <strong>2101195475 / 2010</strong>
-            (preferujeme),{' '}
-            <strong>do zprávy pro příjemce napište název akce</strong>.
-            Nebo hotově správci zkušebny.
-          </li>
-        </ol>
+        <Box mb={4}>
+          <Stepper activeStep={activeStep} steps={steps} />
+        </Box>
+        {activeStep === 0 ? (
+          <>
+            <Typography variant='h6'>Jednodenní rezervace</Typography>
+            <Typography>
+              Pokud chcete vytvořit rezervaci na jediný den, stačí kliknout na
+              políčko v kalendáři.
+            </Typography>
+            <Image
+              src='/howto/single-day-pick.webp'
+              alt='Jednodenní rezervace'
+              width={536}
+              height={341}
+            />
+          </>
+        ) : activeStep === 1 ? (
+          <>
+            <Typography variant='h6'>Vícedenní rezervace</Typography>
+            <Typography>
+              Pro rezervaci na více dní můžete kliknout a táhnout myší.
+            </Typography>
+            <Image
+              src='/howto/multiple-days-pick.webp'
+              alt='Vícedenní rezervace'
+              width={536}
+              height={341}
+            />
+          </>
+        ) : activeStep === 2 ? (
+          <>
+            <Typography variant='h6'>Výběr času</Typography>
+            <Typography>
+              V rezervaci můžete dodatečně upravit datum i čas
+            </Typography>
+            <Image
+              src='/howto/time-pick.webp'
+              alt='Výběr času'
+              width={536}
+              height={341}
+            />
+          </>
+        ) : activeStep === 3 ? (
+          <>
+            <Typography variant='h6'>Výběr položek</Typography>
+            <Typography>
+              Již zarezervované položky se zobrazí, ale není možné je vybrat.
+            </Typography>
+            <Typography>
+              Vyberte položky, které potřebujete a klikněte na tlačítko{' '}
+              <strong>Rezervovat</strong>
+            </Typography>
+            <Typography>
+              Vytvoření rezervace může chvíli trvat, mějte prosím strpení a
+              vyčkejte, dokud se rezervační okno nezavře.
+            </Typography>
+            <Typography>
+              Měli byste poté vidět svojí rezervaci v kalendáři
+            </Typography>
+            <Image
+              src='/howto/items-pick.webp'
+              alt='Výběr položek'
+              width={536}
+              height={341}
+            />
+          </>
+        ) : (
+          <>
+            <Typography variant='h6'>Potvrzující email</Typography>
+            <Typography>Poté vyčkejte na potvrzující email.</Typography>
+            <Image
+              src='/howto/confirm-email.webp'
+              alt='Potvrzující email'
+              width={536}
+              height={341}
+            />
+          </>
+        )}
       </Modal.Content>
+      <Modal.Actions>
+        <Stack justifyContent='space-between' direction='row'>
+          <Button
+            disabled={activeStep === 0}
+            variant='outlined'
+            onClick={handleBack}
+          >
+            Předchozí
+          </Button>
+          <Button
+            disabled={activeStep >= steps.length - 1}
+            variant='outlined'
+            onClick={handleNext}
+          >
+            Další
+          </Button>
+        </Stack>
+      </Modal.Actions>
     </Modal>
   )
 }
