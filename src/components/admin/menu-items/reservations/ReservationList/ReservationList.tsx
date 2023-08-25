819be848f59063b009ea2ba4a7cb557a894ddd1b
Ondřej Tůma <ondrej.tuma@gmail.com>
2023-08-25 17:04:13 +0200

    feat(ReservationList): differentiate repeat reservation in reservation list

--- src/components/admin/menu-items/reservations/ReservationList/ReservationList.tsx
+++ src/components/admin/menu-items/reservations/ReservationList/ReservationList.tsx
@@ -1,4 +1,3 @@
-import EmptyList from 'Components/admin/EmptyList'
 import React from 'react'
 import {
   BooleanField,
@@ -6,36 +5,81 @@ import {
   CreateButton,
   Datagrid,
   DateField,
-  List, NumberField,
+  FunctionField,
+  List,
+  NumberField,
   ReferenceField,
-  TextField, TextInput, TopToolbar,
+  TextField,
+  TextInput,
+  TopToolbar,
 } from 'react-admin'
+import CloseIcon from '@mui/icons-material/Close'
+import CheckIcon from '@mui/icons-material/Check'
+import { Typography } from '@mui/material'
+
+import EmptyList from 'Components/admin/EmptyList'
+import { Reservation } from 'LocalTypes'
 
 const filters = [
-  <TextInput key="title" source="title" label="Název" alwaysOn/>,
-  <BooleanInput key="current" source="current" label="Skrýt proběhlé" alwaysOn/>,
+  <TextInput key='title' source='title' label='Název' alwaysOn />,
+  <BooleanInput
+    key='current'
+    source='current'
+    label='Skrýt proběhlé'
+    alwaysOn
+  />,
 ]
 
 const ListActions = () => (
   <TopToolbar>
-    <CreateButton label={'Vytvořit novou rezervaci'}/>
+    <CreateButton label={'Vytvořit novou rezervaci'} />
   </TopToolbar>
 )
 
 const ReservationList = (props: object) => {
   return (
-    <List {...props} actions={<ListActions />} empty={<EmptyList/>} filters={filters}>
-      <Datagrid rowClick="edit">
-        <TextField label="Název" source="reservationName"/>
-        <TextField label="Jméno" source="name"/>
-        <TextField label="E-mail" source="email"/>
-        <TextField label="Telefon" source="phone"/>
-        <NumberField label="Cena" source="price" locales="cs-CZ" options={{ style: 'currency', currency: 'CZK', maximumFractionDigits: 0 }}/>
-        <BooleanField label="Archivovaná" source="archived"/>
-        <BooleanField label="Zaplacená" source="paid"/>
-        <DateField label="Začátek" source="dateStart"/>
-        <ReferenceField label="Účel rezervace" reference="reservation-types" source="reservationType">
-          <TextField source="title"/>
+    <List
+      {...props}
+      actions={<ListActions />}
+      empty={<EmptyList />}
+      filters={filters}
+    >
+      <Datagrid rowClick='edit'>
+        <TextField label='Název' source='reservationName' />
+        <TextField label='Jméno' source='name' />
+        <TextField label='E-mail' source='email' />
+        <TextField label='Telefon' source='phone' />
+        <NumberField
+          label='Cena'
+          source='price'
+          locales='cs-CZ'
+          options={{
+            style: 'currency',
+            currency: 'CZK',
+            maximumFractionDigits: 0,
+          }}
+        />
+        <FunctionField
+          label='Opakující se'
+          render={({ recurringEventId }: Reservation) => (
+            <Typography sx={{ lineHeight: 0 }}>
+              {recurringEventId ? (
+                <CheckIcon fontSize='small' />
+              ) : (
+                <CloseIcon fontSize='small' />
+              )}
+            </Typography>
+          )}
+        />
+        <BooleanField label='Archivovaná' source='archived' />
+        <BooleanField label='Zaplacená' source='paid' />
+        <DateField label='Začátek' source='dateStart' />
+        <ReferenceField
+          label='Účel rezervace'
+          reference='reservation-types'
+          source='reservationType'
+        >
+          <TextField source='title' />
         </ReferenceField>
       </Datagrid>
     </List>
